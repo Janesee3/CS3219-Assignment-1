8874452b6ac144e45721fd35ff587fb53f1f5e22
See Loo Jane <muigek34@yahoo.com.sg>
2017-01-25 11:26:44 +0800

    Completed InputHandler code

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -1,7 +1,14 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class InputHandler {
-
+	
+	private final static String MSG_INPUT_TITLE_NUMBER = "Please key in the number of movie titles, followed by Return key.";
+	private final static String MSG_INPUT_TITLE = "Please key in the movie titles, ending each with Return key.";
+	private final static String MSG_INPUT_IGNORE_WORDS = "Please key in the words to ignore, separate each word with a single spacing, followed by Return key.";
+	
 	/**
 	 *  Sets the input tokenizedTitles list with multiple entries of string arrays
 	 *  each representing an input movie title 
@@ -9,8 +16,47 @@ public class InputHandler {
 	 * 							the tokenised input titles
 	 * @param ignoreList		Reference to an ArrayList of input words to ignore
 	 */
-	public static void readAndStoreInput(ArrayList<ArrayList<String>> tokenizedTitles, ArrayList<String> ignoreList) {
-		return;
+	public static void readAndStoreInput(	ArrayList<ArrayList<String>> tokenizedTitles, 
+											ArrayList<String> ignoreList) {
+		
+		Scanner scanner = new Scanner(System.in);
+		readTitlesInput(tokenizedTitles, scanner);
+		readIgnoreWordsInput(ignoreList, scanner);
+	}
+	
+	private static void readTitlesInput(ArrayList<ArrayList<String>> tokenizedTitles,
+										Scanner scanner) {
+		
+		// Prompt for number of titles
+		System.out.println(MSG_INPUT_TITLE_NUMBER);
+		
+		int counter = 0;
+		int numLines = scanner.nextInt();
+		
+		// Prompt for titles
+		System.out.println(MSG_INPUT_TITLE);
+		
+		// Read, parse and store
+		while (counter < numLines) {
+			tokenizedTitles.add(readLineAndParse(scanner));
+		}
+	}
+	
+	private static void readIgnoreWordsInput(ArrayList<String> ignoreList, Scanner scanner) {
+		
+		// Prompt for words to ignore
+		System.out.println(MSG_INPUT_IGNORE_WORDS);
+		
+		// Read, parse and update
+		ignoreList = readLineAndParse(scanner);
+	}
+	
+	// Reads in a line through the input scanner, parse the words with regex "\\s+",
+	// and returns an ArrayList of the individual word strings.
+	private static ArrayList<String> readLineAndParse(Scanner scanner) {
+		String line = scanner.nextLine();
+		String[] parsedLine = line.split("\\s+"); 
+		return new ArrayList<String>(Arrays.asList(parsedLine));
 	}
 	
 	
